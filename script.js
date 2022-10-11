dfff37788f02f2e797714018914e06efe4739e6f
SAISRAVAN <oletisravan@gmail.com>
2022-10-11 19:23:13 +0530

    Sravan : LOL Spelling mistake

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-model');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-   btnsOpenModal[i].addEventListener('click', function () {
-      modal.classList.remove('hidden');
-      overlay.classList.remove('hidden');
-   });
-}
+const openModal = function () {
+   modal.classList.remove('hidden');
+   overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
    modal.classList.add('hidden');
    overlay.classList.add('hidden');
-}); 
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
