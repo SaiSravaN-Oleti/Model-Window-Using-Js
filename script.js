f88f1e0203190014bfc9b7261eafa777cba35bff
SAISRAVAN <oletisravan@gmail.com>
2022-10-11 19:00:37 +0530

    Sravan : click open and overlay bg

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
    btnsOpenModal[i].addEventListener('click', function () {
-      console.log('Button Clicked');
       modal.classList.remove('hidden');
       overlay.classList.remove('hidden');
    });
 }
+
+btnCloseModal.addEventListener('click', function () {
+   modal.classList.add('hidden');
+   overlay.classList.add('hidden');
+}); 
